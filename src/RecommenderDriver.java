8ef780c09bdffa660597a6634b19398ec426d3d6
Lakshay Nagpal <lakshay.nagpal00000@gmail.com>
2016-01-03 01:22:09 +0530

    Updated Driver with Tool Runner

--- src/RecommenderDriver.java
+++ src/RecommenderDriver.java
@@ -1,22 +1,24 @@
+
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
 
 
-public class RecommenderDriver {
+public class RecommenderDriver extends Configured implements Tool{
 
-public static void main(String [] args) throws Exception {
+@Override
+	public int run(String[] args) throws Exception{
 	
-if(args.length!=2){
+	Configuration conf = getConf();
 	
-	System.err.println("Usage: RecommenderDriver <input path> <output path>");
-	System.exit(-1);
-}
-
-Job job = new Job();
+Job job = new Job(conf, "My Job");
 job.setJarByClass(RecommenderDriver.class);
 job.setJobName("Recommender System");
 
@@ -29,6 +31,14 @@ job.setReducerClass(RecommenderReducer.class);
 job.setOutputKeyClass(Text.class);
 job.setOutputValueClass(IntWritable.class);
 
-System.exit(job.waitForCompletion(true)?0:1);
-		}	
-	}
+return job.waitForCompletion(true)?0:1 ; 
+}	
+
+
+public static void main(String[] args) throws Exception {
+
+	 int exitCode=ToolRunner.run(new Configuration(), new RecommenderDriver(), args);
+			  System.exit(exitCode);
+
+}
+}
